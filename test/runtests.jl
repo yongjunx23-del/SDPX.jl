6994c89227f9dfe626bbedd9f39a05027f208a33
Yongjun Xu <yongjunx23@gmail.com>
2026-09-09 18:07:28 +0800

    test(r5): wire R5-A/R5-B into the suite; allowlist standalone R4-A pairing matrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -664,6 +664,8 @@ include(joinpath(@__DIR__, "factor_pair_public_qualification.jl"))
 include(joinpath(@__DIR__, "factor_pair_mirror_drift.jl"))
 include(joinpath(@__DIR__, "accuracy_contract.jl"))
 include(joinpath(@__DIR__, "test_r1_full_qualification.jl"))
+include(joinpath(@__DIR__, "test_r5a_precision_controller.jl"))
+include(joinpath(@__DIR__, "test_r5b_thread_budget.jl"))
 include(joinpath(@__DIR__, "..", "validation", "scientific_core",
     "test_r2a_symbolic_numeric_separation.jl"))
 include(joinpath(@__DIR__, "session_symbolic_lease.jl"))
